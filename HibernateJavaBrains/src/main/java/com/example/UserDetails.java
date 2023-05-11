8fde1e1cd88908388846d0e7e4e3241af292361d
Kuba1618 <jakub.prusicki1.618@gmail.com>
2023-05-11 22:46:26 +0200

    @GenericGenerator works fine

--- HibernateJavaBrains/src/main/java/com/example/UserDetails.java
+++ HibernateJavaBrains/src/main/java/com/example/UserDetails.java
@@ -1,7 +1,7 @@
 package com.example;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
@@ -12,6 +12,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 
 @Entity 
@@ -26,7 +30,9 @@ public class UserDetails {
 	@ElementCollection
 	@JoinTable(name="USER_ADDRESS",
 			joinColumns = @JoinColumn(name="USER_ID"))
-	private Set<Address> listOfAddresses = new HashSet();
+	@GenericGenerator(name="sequence-gen",strategy="increment")
+	@CollectionId(columns = { @Column (name = "ADDRESS_ID")}, generator = "sequence-gen", type = @Type(type = "long"))
+	private Collection<Address> listOfAddresses = new ArrayList<Address>();
 	
 	
 	public int getUserId() {
@@ -46,11 +52,11 @@ public class UserDetails {
 		this.userName = userName;
 	}
 
-	public Set<Address> getListOfAddresses() {
+	public Collection<Address> getListOfAddresses() {
 		return listOfAddresses;
 	}
 
-	public void setListOfAddresses(Set<Address> listOfAddresses) {
+	public void setListOfAddresses(Collection<Address> listOfAddresses) {
 		this.listOfAddresses = listOfAddresses;
 	}
 	
