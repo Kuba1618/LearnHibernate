a44bd0b7f9d7a88ffc3c001c480d549630ac3fd6
Kuba1618 <jakub.prusicki1.618@gmail.com>
2023-05-16 20:03:01 +0200

    Implementing inheritance without any special annotation

--- HibernateJavaBrains/src/test/java/com/example/HibernateTest.java
+++ HibernateJavaBrains/src/test/java/com/example/HibernateTest.java
@@ -11,25 +11,26 @@ import com.github.javafaker.Faker;
 public class HibernateTest {
 
 	public static void main(String[] args) {
-		Faker faker = new Faker(new Locale("pl-PL"));
-		
-		UserDetails user = new UserDetails();
-		user.setUserName(faker.artist().name());
+		//Faker faker = new Faker(new Locale("pl-PL"));
 		
 		Vehicle vehicle = new Vehicle();
 		vehicle.setVehicleName("Car");
 		
-		Vehicle vehicle2 = new Vehicle();
-		vehicle2.setVehicleName("Jeep");
+		TwoWheeler bike = new TwoWheeler();
+		bike.setVehicleName("bike");
+		bike.setSteeringHandle("Bike steering handle");
 		
-		user.getVehicle().add(vehicle);
-		user.getVehicle().add(vehicle2);
+		FourWheeler car = new FourWheeler();
+		car.setVehicleName("Porsche");
+		car.setSteeringWheel("Porsche steering wheel");
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		
 		session.beginTransaction();
-		session.persist(user);
+		session.save(vehicle);
+		session.save(bike);
+		session.save(car);
 		
 		session.getTransaction().commit();
 		
