7c5feca26f4f7510df88e409a4770deb14cfbc3b
Kuba1618 <jakub.prusicki1.618@gmail.com>
2023-05-16 20:52:04 +0200

    Continue Single Table Inheritance & set discriminator value.

--- HibernateJavaBrains/src/main/java/com/example/Vehicle.java
+++ HibernateJavaBrains/src/main/java/com/example/Vehicle.java
@@ -1,14 +1,19 @@
 package com.example;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(
+		name="VEHICLE_TYPE",
+		discriminatorType=DiscriminatorType.STRING
+)
 public class Vehicle {
 	@Id @GeneratedValue
 	private int vehicleId;
